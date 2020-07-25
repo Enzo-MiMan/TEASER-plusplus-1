826c21803b161348708fdb8fb7f477fa83418823
Enzo <53284465+Enzo-MiMan@users.noreply.github.com>
2020-07-25 21:44:09 +0100

    Update teaser_python_ply.py

--- examples/teaser_python_ply/teaser_python_ply.py
+++ examples/teaser_python_ply/teaser_python_ply.py
@@ -1,8 +1,13 @@
 import copy
 import time
+import os
+import re
 import numpy as np
+from os.path import join
 import open3d as o3d
 import teaserpp_python
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 NOISE_BOUND = 0.05
 N_OUTLIERS = 1700
@@ -14,75 +19,68 @@ def get_angular_error(R_exp, R_est):
     """
     Calculate angular error
     """
-    return abs(np.arccos(min(max(((np.matmul(R_exp.T, R_est)).trace() - 1) / 2, -1.0), 1.0)));
+    return abs(np.arccos(min(max(((np.matmul(R_exp.T, R_est)).trace() - 1) / 2, -1.0), 1.0)))
+
+
+def extract_pc(content):
+
+    timestamp = int(re.split('\s+', content)[0])
+    num_point = (len(re.split('\s+', content)) - 2) / 3
+
+    point = []
+    for i in range(int(num_point)):
+        x = float(re.split('\s+', content)[i * 3 + 1])
+        y = float(re.split('\s+', content)[i * 3 + 2])
+        z = float(re.split('\s+', content)[i * 3 + 3])
+        point.append((x, y, z))
+    point = np.array(point)
+
+    return point
 
 
 if __name__ == "__main__":
-    print("==================================================")
-    print("        TEASER++ Python registration example      ")
-    print("==================================================")
 
-    # Load bunny ply file
-    src_cloud = o3d.io.read_point_cloud("../example_data/bun_zipper_res3.ply")
-    src = np.transpose(np.asarray(src_cloud.points))
-    N = src.shape[1]
+    with open('/Users/manmi/Documents/data/square_data/mm_data/mm_src_sample_corres.txt') as file:
+        content_src = file.readlines()
 
-    # Apply arbitrary scale, translation and rotation
-    T = np.array(
-        [[9.96926560e-01, 6.68735757e-02, -4.06664421e-02, -1.15576939e-01],
-         [-6.61289946e-02, 9.97617877e-01, 1.94008687e-02, -3.87705398e-02],
-         [4.18675510e-02, -1.66517807e-02, 9.98977765e-01, 1.14874890e-01],
-         [0, 0, 0, 1]])
+    with open('/Users/manmi/Documents/data/square_data/mm_data/mm_dts_sample_corres.txt') as file:
+        content_tgt = file.readlines()
 
-    dst_cloud = copy.deepcopy(src_cloud)
-    dst_cloud.transform(T)
-    dst = np.transpose(np.asarray(dst_cloud.points))
+    for i in range(len(content_tgt)):
+    # for i in range(1):
 
-    # Add some noise
-    dst += (np.random.rand(3, N) - 0.5) * 2 * NOISE_BOUND
+        src = extract_pc(content_src[i])
+        tgt = extract_pc(content_tgt[i])
 
-    # Add some outliers
-    outlier_indices = np.random.randint(N_OUTLIERS, size=N_OUTLIERS)
-    for i in range(outlier_indices.size):
-        shift = OUTLIER_TRANSLATION_LB + np.random.rand(3, 1) * (OUTLIER_TRANSLATION_UB - OUTLIER_TRANSLATION_LB)
-        dst[:, outlier_indices[i]] += shift.squeeze()
+        assert len(src) == len(tgt)
 
-    # Populating the parameters
-    solver_params = teaserpp_python.RobustRegistrationSolver.Params()
-    solver_params.cbar2 = 1
-    solver_params.noise_bound = NOISE_BOUND
-    solver_params.estimate_scaling = False
-    solver_params.rotation_estimation_algorithm = teaserpp_python.RobustRegistrationSolver.ROTATION_ESTIMATION_ALGORITHM.GNC_TLS
-    solver_params.rotation_gnc_factor = 1.4
-    solver_params.rotation_max_iterations = 100
-    solver_params.rotation_cost_threshold = 1e-12
+        src = np.transpose(src)
+        dst = np.transpose(tgt)
 
-    solver = teaserpp_python.RobustRegistrationSolver(solver_params)
-    start = time.time()
-    solver.solve(src, dst)
-    end = time.time()
+        # Populating the parameters
+        solver_params = teaserpp_python.RobustRegistrationSolver.Params()
+        solver_params.cbar2 = 1
+        solver_params.noise_bound = NOISE_BOUND
+        solver_params.estimate_scaling = False
+        solver_params.rotation_estimation_algorithm = teaserpp_python.RobustRegistrationSolver.ROTATION_ESTIMATION_ALGORITHM.GNC_TLS
+        solver_params.rotation_gnc_factor = 1.4
+        solver_params.rotation_max_iterations = 100
+        solver_params.rotation_cost_threshold = 1e-12
 
-    solution = solver.getSolution()
+        solver = teaserpp_python.RobustRegistrationSolver(solver_params)
+        start = time.time()
+        solver.solve(src, dst)
+        end = time.time()
 
-    print("=====================================")
-    print("          TEASER++ Results           ")
-    print("=====================================")
+        solution = solver.getSolution()
 
-    print("Expected rotation: ")
-    print(T[:3, :3])
-    print("Estimated rotation: ")
-    print(solution.rotation)
-    print("Error (deg): ")
-    print(get_angular_error(T[:3,:3], solution.rotation))
+        T = solution.translation
+        R = solution.rotation
 
-    print("Expected translation: ")
-    print(T[:3, 3])
-    print("Estimated translation: ")
-    print(solution.translation)
-    print("Error (m): ")
-    print(np.linalg.norm(T[:3, 3] - solution.translation))
+        with open('/Users/manmi/Documents/data/square_data/mm_data/mm_T_TEASTER.txt', 'a+') as myfile:
+            myfile.write(str(T[0]) + ' ' + str(T[1]) + ' ' + str(T[2]) + '\n')
 
-    print("Number of correspondences: ", N)
-    print("Number of outliers: ", N_OUTLIERS)
-    print("Time taken (s): ", end - start)
-
+        with open('/Users/manmi/Documents/data/square_data/mm_data/mm_R_matrix_TEASER.txt', 'a+') as myfile:
+            myfile.write(str(R[0, 0]) + ' ' + str(R[0, 1]) + ' ' + str(R[0, 2]) + ' ' +
+                         str(R[1, 0]) + ' ' + str(R[1, 1]) + ' ' + str(R[1, 2]) + ' ' +
+                         str(R[2, 0]) + ' ' + str(R[2, 1]) + ' ' + str(R[2, 2]) + '\n')
